2078d2a01721f519b8a606c0d6cec55696debbcd
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-12-14 17:55:34 -0500

    Add context and stylesheet

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,27 @@
 import { useContext } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+import { AppContext } from './AppContext'
 
 
 const Home = () => {
-    <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-    </View>
-
+    const context = useContext(AppContext)
+    return (
+        <View style={styles.container}>
+            <Text>{context.test}</Text>
+        </View>
+    )
 }
 
 export default  Home
+
+const styles = StyleSheet.create({
+    container: {
+      flex: .2,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderWidth:'2px',
+      borderBottomColor: 'red'
+    },
+  })
