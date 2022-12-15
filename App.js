29943e0a299f506780ef25738b77600b9090ece4
ameliaeiger <98718782+ameliaeiger@users.noreply.github.com>
2022-12-15 14:01:51 -0500

    Add context. Add routing paths. Add view components.

--- App.js
+++ App.js
@@ -1,36 +1,72 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import { NavigationContainer, useNavigation } from '@react-navigation/native'
-import { createNativeStackNavigator} from '@react-navigation/native-stack'
+import { useEffect, useState } from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 // Components
-import { AppContext } from './src/components/AppContext.js'
-import Home from './src/components/Home.js'
+import { AppContext } from "./src/components/AppContext.js"
+import Home from "./src/components/Home.js"
+import Card from "./src/components/TarotCard.js"
+import CardDetails from "./src/components/CardDetails.js"
 
 // Data files
-import deck from "./src/data/tarot-deck.js"
+import tarotData from "./src/data/tarot-deck.js"
 
 // React Navigation
 const Stack = createNativeStackNavigator()
 
 const App = () => {
-  const context = {
-    test: "hello"
+  const deck = tarotData.cards
+  const [cardNames, setCardNames] = useState("")
+  const [id, setId] = useState("")
+  const [selected, setSelected] = useState("")
+
+  const randomNum = (max) => {
+    return Math.floor(Math.random() * max)
   }
 
-  const HomeScreen = ({navigation}) => {
-    return(
-      <Home navigation={navigation} />
-    )
+  const context = {
+    randomNum: randomNum,
+
+    deck: deck,
+    cardNames: cardNames,
+
+    id: id,
+    setId: setId,
+
+    selected: selected,
+    setSelected: setSelected,
   }
-  
-  
+
+  // useEffect(() => {
+  //   setContext()
+  // }, [])
+
+  // const setContext = () => {
+    // let names = deck.map((item) => {
+    //   return item.name
+    // })
+    // setCardNames(names)
+  // }
+
+  const HomeScreen = ({ navigation }) => {
+    return <Home navigation={navigation} />
+  }
+
+  const CardScreen = ({ navigation }) => {
+    return <Card navigation={navigation} />
+  }
+
+  const CardDetailsScreen = ({ navigation }) => {
+    return <CardDetails navigation={navigation} />
+  }
+
   return (
     <AppContext.Provider value={context}>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='Home' component={HomeScreen} 
-          // options={logoutOption} 
-          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Card" component={CardScreen} />
+          <Stack.Screen name="CardDetails" component={CardDetailsScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </AppContext.Provider>
@@ -38,12 +74,3 @@ const App = () => {
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
